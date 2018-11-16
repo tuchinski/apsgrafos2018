c5cd814ee0b9e147738153a3f61853a2b1ce867a
tuchinski <tuchinski@alunos.utfpr.edu.br>
2018-11-16 02:30:01 -0200

    add inicio da api do foursquare

---
+++ apiFoursquare.py
@@ -0,0 +1,38 @@
+import sys
+import requests
+from datetime import datetime
+
+clientIdFoursquare = "Y4NEZOZDM5WW23GKXBJWXA524RR4OAETS3500PTNHLS0HEUC"
+clientSecretFoursquare = "B2K05PB3QO1VWRCHRRSHHU2E2YW4NILFGRI5UWLE2ALSAPK4"
+
+now = datetime.now()
+now = now.date().isoformat()
+now = now.replace('-','')
+
+
+url = "https://api.foursquare.com/v2/venues/explore"
+
+local = input("Digite o local: ")
+
+param = {"client_id": clientIdFoursquare, "client_secret": clientSecretFoursquare,
+         "near": local, "v": now, 'limit':10 }
+
+r = requests.get(url = url, params = param)
+
+dados = r.json()
+dados = dados['response']['groups'][0]['items']
+
+for local in dados:
+    print(local['venue']['name'])
+
+    try:
+        print(local['venue']['location']['address'])
+        
+    except KeyError as identifier:
+        print(local['venue']['location']['lat'])
+        print(local['venue']['location']['lng'])
+        
+
+    print('\n')
+
+# print (len(dados))
--- apiLocalidades.py
+++ apiLocalidades.py
@@ -1,15 +1,35 @@
 import requests
+import sys
 
 urlEstados = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+urlCidades = 'https://servicodados.ibge.gov.br/api/v1/localidades/municipios'
 
-r = requests.get(url=urlEstados)
+r = requests.get(url=urlCidades)
 
 data = r.json()
 
-estado = input("Digite o nome de um estado: ")
+cidade = " ".join(sys.argv[1:])
 
 for e in data:
-    if e["nome"].lower() == estado.lower():
-        print(e)
+    if e["nome"].lower() == cidade.lower():
+        # print(str(e))
+        cidade = e
+        break
+# print (cidade)
 
-# print (data)
+try:
+        urlMicrorregioes = 'https://servicodados.ibge.gov.br/api/v1/localidades/microrregioes/%s/municipios' % cidade['microrregiao']['id']
+except TypeError as identifier:
+        print("A cidade não foi encontrada")
+        sys.exit(0)
+
+reqMicrorregioes = requests.get(url=urlMicrorregioes)
+
+cidadesMicrorregiao = reqMicrorregioes.json()
+
+for c in cidadesMicrorregiao:
+        # c['distancia'] = 100
+        print (c['id'])
+        print (c['nome'])
+        print (c['distancia'])
+        print ("\n")
